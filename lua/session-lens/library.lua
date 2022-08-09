0a850ecba1d5450913813d9bb23255a118456f76
Ronnie Magatti <ronniemagatti@gmail.com>
2022-08-08 23:59:05 -0600

    Use native neovim functions

--- lua/session-lens/library.lua
+++ lua/session-lens/library.lua
@@ -1,18 +1,18 @@
-local path = require('plenary.path')
-local AutoSession = require('auto-session')
-local AutoSessionLib = require('auto-session-library')
+local path = require "plenary.path"
+local AutoSession = require "auto-session"
+local AutoSessionLib = require "auto-session-library"
 
 local Config = {}
 local Lib = {
   make_entry = {},
   logger = {},
   conf = {
-    logLevel = false
+    logLevel = false,
   },
   Config = Config,
   _VIM_FALSE = 0,
-  _VIM_TRUE  = 1,
-  ROOT_DIR = AutoSession.conf.auto_session_root_dir
+  _VIM_TRUE = 1,
+  ROOT_DIR = AutoSession.conf.auto_session_root_dir,
 }
 
 -- Setup ======================================================
@@ -20,35 +20,20 @@ function Lib.setup(config)
   Lib.conf = vim.tbl_deep_extend("force", Lib.conf, config)
 end
 
--- Helper functions ===============================================================
-local function hasValue (tab, val)
-  for _, value in ipairs(tab) do
-    if value == val then
-      return true
-    end
-  end
-
-  return false
-end
-
 function Lib.isEmpty(s)
-  return s == nil or s == ''
-end
-
-function Lib.endsWith(str, ending)
-  return ending == "" or str:sub(-#ending) == ending
+  return s == nil or s == ""
 end
 
 function Lib.appendSlash(str)
   if not Lib.isEmpty(str) then
-    if not Lib.endsWith(str, "/") then
-      str = str.."/"
+    if not vim.endswith(str, "/") then
+      str = str .. "/"
     end
   end
   return str
 end
--- ===================================================================================
 
+-- ===================================================================================
 
 -- ==================== SessionLens ==========================
 function Lib.make_entry.gen_from_file(opts)
@@ -60,38 +45,40 @@ function Lib.make_entry.gen_from_file(opts)
       filename = line,
       cwd = root,
       display = function(_)
-        local out = AutoSessionLib.unescape_dir(line):match("(.+)%.vim")
+        local out = AutoSessionLib.unescape_dir(line):match "(.+)%.vim"
         if opts.path_display and vim.tbl_contains(opts.path_display, "shorten") then
           out = path:new(out):shorten()
         end
-        if out then return out end
+        if out then
+          return out
+        end
         return line
       end,
-      path = path:new(root, line):absolute()
+      path = path:new(root, line):absolute(),
     }
   end
 end
+
 -- ===================================================================================
 
 -- Logger =========================================================
 function Lib.logger.debug(...)
-  if Lib.conf.logLevel == 'debug' then
+  if Lib.conf.logLevel == "debug" then
     print(...)
   end
 end
 
 function Lib.logger.info(...)
-  local valid_values = {'info', 'debug'}
-  if hasValue(valid_values, Lib.conf.logLevel) then
+  local valid_values = { "info", "debug" }
+  if vim.tbl_contains(valid_values, Lib.conf.logLevel) then
     print(...)
   end
 end
 
 function Lib.logger.error(...)
-   error(...)
+  error(...)
 end
+
 -- =========================================================
 
-
 return Lib
-
