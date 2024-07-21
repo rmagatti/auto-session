ba32f1d16b4d13a8975851e8a59e78c8f1f5048a
Cameron Ring <cameron@cs.stanford.edu>
2024-07-20 22:30:23 -0700

    test: legacy session delete

--- tests/legacy_file_names_spec.lua
+++ tests/legacy_file_names_spec.lua
@@ -7,57 +7,6 @@ describe("Legacy file name support", function()
     -- log_level = "debug",
   }
 
-  local Lib = as.Lib
-
-  it("can convert old session file names to new format", function()
-    TL.clearSessionFilesAndBuffers()
-
-    -- save a default session
-    as.SaveSession()
-    as.SaveSession(TL.named_session_name)
-
-    assert.equals(1, vim.fn.filereadable(TL.default_session_path))
-    assert.equals(1, vim.fn.filereadable(TL.named_session_path))
-
-    local old_sessions
-    if vim.fn.has "win32" then
-      old_sessions = {
-        "C++-Users-home-a.vim",
-        "C++-Users-home-b.vim",
-        "C++-Users-home-c.vim",
-        "C++-Users-home-123.vim",
-        "C++-Users-home-dash-tiest.vim",
-        "C++-Users-home-123-otherdir.vim",
-        "C++-Users-home-dash-test-otherdir.vim",
-      }
-    else
-      old_sessions = {
-        "%Users%home%a.vim",
-        "%Users%home%b.vim",
-        "%Users%home%c.vim",
-        "%Users%home%123.vim",
-        "%Users%home%dash-tiest.vim",
-        "%Users%home%123%otherdir.vim",
-        "%Users%home%dash-test%otherdir.vim",
-      }
-    end
-
-    for _, file_name in ipairs(old_sessions) do
-      TL.createFile(TL.session_dir .. file_name)
-      assert.equals(1, vim.fn.filereadable(TL.session_dir .. file_name))
-    end
-
-    Lib.convert_session_dir(TL.session_dir)
-
-    for _, old_file_name in ipairs(old_sessions) do
-      print(old_file_name)
-      assert.equals(0, vim.fn.filereadable(TL.session_dir .. old_file_name))
-    end
-
-    assert.equals(1, vim.fn.filereadable(TL.default_session_path))
-    assert.equals(1, vim.fn.filereadable(TL.named_session_path))
-  end)
-
   it("can convert a session to the new format during a restore", function()
     TL.clearSessionFilesAndBuffers()
 
@@ -84,10 +33,37 @@ describe("Legacy file name support", function()
     -- did we successfully restore the session?
     assert.equals(1, vim.fn.bufexists(TL.test_file))
 
-    -- did we know have a new filename?
+    -- did we now have a new filename?
     assert.equals(1, vim.fn.filereadable(TL.default_session_path))
 
     -- and no old file name?
+    assert.equals(0, vim.fn.filereadable(TL.default_session_path_legacy))
+  end)
+
+  it("can convert a session to the new format during a delete", function()
+    TL.clearSessionFilesAndBuffers()
+
+    vim.cmd("e " .. TL.test_file)
+
+    assert.equals(1, vim.fn.bufexists(TL.test_file))
+
+    -- save a default session in new format
+    as.SaveSession()
+    assert.equals(1, vim.fn.filereadable(TL.default_session_path))
+
+    vim.loop.fs_rename(TL.default_session_path, TL.default_session_path_legacy)
+
+    print(TL.default_session_path_legacy)
+    assert.equals(1, vim.fn.filereadable(TL.default_session_path_legacy))
+    assert.equals(0, vim.fn.filereadable(TL.default_session_path))
+
+    vim.cmd "%bw!"
+
+    assert.equals(0, vim.fn.bufexists(TL.test_file))
+
+    as.DeleteSession()
+
+    -- file should be gone
     assert.equals(0, vim.fn.filereadable(TL.default_session_path_legacy))
   end)
 end)
