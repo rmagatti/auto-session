e3e990eafba2ab695bc4e14d484aa729b4412a0d
Cameron Ring <cameron@cs.stanford.edu>
2024-07-31 23:46:00 -0700

    test: turn off auto save/restore for ui tests
    Prevents default session from being saved empty which could throw off
    the tests

--- tests/mini-tests/test_ui.lua
+++ tests/mini-tests/test_ui.lua
@@ -1,4 +1,4 @@
----@diagnostic disable: undefined-field
+---@diagnostic disable: undefined-field, undefined-global
 -- Define helper aliases
 local new_set = MiniTest.new_set
 local expect, eq = MiniTest.expect, MiniTest.expect.equality
@@ -6,24 +6,29 @@ local expect, eq = MiniTest.expect, MiniTest.expect.equality
 -- Create (but not start) child Neovim object
 local child = MiniTest.new_child_neovim()
 
+local TL = require "tests/test_lib"
+
 -- Define main test set of this file
 local T = new_set {
   -- Register hooks
   hooks = {
+    pre_once = function()
+      TL.clearSessionFilesAndBuffers()
+    end,
     -- This will be executed before every (even nested) case
     pre_case = function()
       -- Restart child process with custom 'init.lua' script
       child.restart { "-u", "scripts/minimal_init_mini.lua" }
       -- Load tested plugin
-      child.lua [[M = require('auto-session').setup()]]
+      child.lua [[M = require('auto-session').setup({
+        auto_save_enabled = false,
+        auto_restore_enabled = false,
+      })]]
     end,
     -- This will be executed one after all tests from this set are finished
     post_once = child.stop,
   },
 }
-
-local TL = require "tests/test_lib"
-TL.clearSessionFilesAndBuffers()
 
 T["session lens"] = new_set {}
 
@@ -42,7 +47,7 @@ T["session lens"]["save a named session"] = function()
   child.cmd("SessionSave " .. TL.named_session_name)
   expect.equality(1, vim.fn.filereadable(TL.named_session_path))
 
-  -- to make sure we can list sessions ending in x (checked in test below)
+  child.cmd("e " .. TL.other_file)
   child.cmd "SessionSave project_x"
 end
 
@@ -51,10 +56,11 @@ T["session lens"]["can load a session"] = function()
   child.cmd "SessionSearch"
   -- give the UI time to come up
   vim.loop.sleep(100)
+  child.type_keys "project_x"
   child.type_keys "<cr>"
   -- give the session time to load
   vim.loop.sleep(500)
-  expect.equality(1, child.fn.bufexists(TL.test_file))
+  expect.equality(1, child.fn.bufexists(TL.other_file))
 end
 
 T["session lens"]["can delete a session"] = function()
