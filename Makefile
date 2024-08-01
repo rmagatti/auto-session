2e642b77b8ed330d5b7f6d55881e42bed6918263
Cameron Ring <cameron@cs.stanford.edu>
2024-07-31 20:06:49 -0700

    test: trying out mini.test for UI end to end testing

--- Makefile
+++ Makefile
@@ -8,24 +8,35 @@ TELESCOPE_VER = 0.1.8
 TELESCOPE_DIR = .test/telescope
 TELESCOPE_URL = https://github.com/nvim-telescope/telescope.nvim
 
-PLUGINS := $(PLENARY_DIR) $(TELESCOPE_DIR)
+MINI_DIR = .test/mini.nvim
+
+PLUGINS := $(PLENARY_DIR) $(TELESCOPE_DIR) ${MINI_DIR}
 
 FILES := $(wildcard tests/*_spec.lua)
 
-.PHONY: test ${FILES} args-tests
+.PHONY: test ${FILES} plenary-tests mini-tests
+
+test: plenary-tests mini-tests
 
 # It's faster to run the tests via PlenaryBustedDirectory because it only needs one overall, managing nvim process and then one per spec.
 # PlenaryBustedFile, on the other hand, starts two processes per spec (one to manage running the spec and then one to actually run the spec)
 # But it's very convenient to be able to run a single spec when test/developing
-test: $(PLUGINS)
-	nvim --clean --headless -u tests/minimal.lua +"PlenaryBustedDirectory tests {minimal_init = 'tests/minimal.lua', sequential=true}"
-	
+plenary-tests: $(PLUGINS)
+	nvim --clean --headless -u scripts/minimal_init.lua +"PlenaryBustedDirectory tests {minimal_init = 'scripts/minimal_init.lua', sequential=true}"
+
 # Rule that lets you run an individual spec. Currently requires my Plenary fork above
 $(FILES): $(PLUGINS)
-	nvim --clean --headless -u tests/minimal.lua +"PlenaryBustedFile $@ {minimal_init = 'tests/minimal.lua'}"
+	nvim --clean --headless -u scripts/minimal_init.lua +"PlenaryBustedFile $@ {minimal_init = 'scripts/minimal_init.lua'}"
+	
+# We use mini.test for some end to end UI testing of session lens
+mini-tests: $(PLUGINS)
+	nvim --headless --noplugin -u scripts/minimal_init_mini.lua -c "lua MiniTest.run()"
 
 $(PLENARY_DIR):
-	git clone --depth=1 --branch $(PLENARY_VER) $(PLENARY_URL) $(PLENARY_DIR)
+	git clone --depth=1 --branch $(PLENARY_VER) $(PLENARY_URL) $@
 
 $(TELESCOPE_DIR):
-	git clone --depth=1 --branch $(TELESCOPE_VER) $(TELESCOPE_URL) $(TELESCOPE_DIR)
+	git clone --depth=1 --branch $(TELESCOPE_VER) $(TELESCOPE_URL) $@
+
+$(MINI_DIR):
+	git clone --depth=1 --filter=blob:none https://github.com/echasnovski/mini.nvim $@
---
+++ tests/mini-tests/test_ui.lua
@@ -0,0 +1,79 @@
+-- Define helper aliases
+local new_set = MiniTest.new_set
+local expect, eq = MiniTest.expect, MiniTest.expect.equality
+
+-- Create (but not start) child Neovim object
+local child = MiniTest.new_child_neovim()
+
+-- Define main test set of this file
+local T = new_set {
+  -- Register hooks
+  hooks = {
+    -- This will be executed before every (even nested) case
+    pre_case = function()
+      -- Restart child process with custom 'init.lua' script
+      child.restart { "-u", "scripts/minimal_init_mini.lua" }
+      -- Load tested plugin
+      child.lua [[M = require('auto-session').setup()]]
+    end,
+    -- This will be executed one after all tests from this set are finished
+    post_once = child.stop,
+  },
+}
+
+local TL = require "tests/test_lib"
+TL.clearSessionFilesAndBuffers()
+
+T["session lens"] = new_set {}
+
+T["session lens"]["save a default session"] = function()
+  child.cmd("e " .. TL.test_file)
+  expect.equality(1, child.fn.bufexists(TL.test_file))
+  child.cmd "SessionSave"
+
+  expect.equality(1, child.fn.bufexists(TL.test_file))
+  expect.equality(1, vim.fn.filereadable(TL.default_session_path))
+end
+
+T["session lens"]["save a named session"] = function()
+  child.cmd("e " .. TL.test_file)
+  expect.equality(1, child.fn.bufexists(TL.test_file))
+  child.cmd("SessionSave " .. TL.named_session_name)
+  expect.equality(1, vim.fn.filereadable(TL.named_session_path))
+
+  -- to make sure we can list sessions ending in x (checked in test below)
+  child.cmd "SessionSave project_x"
+end
+
+T["session lens"]["can list sessions"] = function()
+  child.cmd "SessionSearch"
+  -- give the UI time to come up
+  vim.loop.sleep(100)
+  expect.reference_screenshot(child.get_screenshot())
+end
+
+T["session lens"]["can load a session"] = function()
+  expect.equality(0, child.fn.bufexists(TL.test_file))
+  child.cmd "SessionSearch"
+  -- give the UI time to come up
+  vim.loop.sleep(100)
+  child.type_keys "<cr>"
+  -- give the session time to load
+  vim.loop.sleep(500)
+  expect.reference_screenshot(child.get_screenshot())
+  expect.equality(1, child.fn.bufexists(TL.test_file))
+end
+
+T["session lens"]["can delete a session"] = function()
+  expect.equality(1, vim.fn.filereadable(TL.named_session_path))
+  child.cmd "SessionSearch"
+  -- give the UI time to come up
+  vim.loop.sleep(100)
+  child.type_keys "mysession"
+  child.type_keys "<c-d>"
+  vim.loop.sleep(100)
+  expect.equality(0, vim.fn.filereadable(TL.named_session_path))
+end
+
+-- Return test set which will be collected and execute inside `MiniTest.run()`
+return T
