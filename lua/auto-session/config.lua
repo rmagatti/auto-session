f783c175878bff221a08f1e75400ce2b71d2f617
Cameron Ring <cameron@cs.stanford.edu>
2024-08-22 22:35:46 -0700

    docs: update default config comments

--- lua/auto-session/config.lua
+++ lua/auto-session/config.lua
@@ -58,39 +58,44 @@ local M = {}
 local defaults = {
   enabled = true, -- Enables/disables auto creating, saving and restoring
   root_dir = vim.fn.stdpath "data" .. "/sessions/", -- Root dir where sessions will be stored
-  auto_save = true, -- Enables/disables auto save feature
-  auto_restore = true, -- Enables/disables auto restore feature
+  auto_save = true, -- Enables/disables auto saving session on exit
+  auto_restore = true, -- Enables/disables auto restoring session on start
   auto_create = true, -- Enables/disables auto creating new session files. Can take a function that should return true/false if a new session file should be created or not
   suppressed_dirs = nil, -- Suppress session restore/create in certain directories
   alloweded_dirs = nil, -- Allow session restore/create in certain directories
-  auto_restore_last_session = false, -- Enables/disables the "last session" feature
+  auto_restore_last_session = false, -- On startup, loads the last saved session if session for cwd does not exist
   use_git_branch = false, -- Include git branch name in session name
-  lazy_support = true, -- Enables/disables Lazy delay feature
-  bypass_save_filetypes = nil, -- Bypass auto save when only buffer open is one of these file types
+  lazy_support = true, -- Automatically detect if Lazy.nvim is being used and wait until Lazy is done to make sure session is restored correctly. Does nothing if Lazy isn't being used. Can be disabled if a problem is suspected or for debugging
+  bypass_save_filetypes = nil, -- List of filetypes to bypass auto save when the only buffer open is one of the file types listed, useful to ignore dashboards
   close_unsupported_windows = true, -- Close windows that aren't backed by normal file before autosaving a session
-  args_allow_single_directory = true, -- Allow single directory arguments by default
-  args_allow_files_auto_save = false, -- Don't save session for file args by default
-  continue_restore_on_error = true, -- Suppress extraneous messages and source the whole session, even if there's an error. Set to false to get the line number of a restore error
-  cwd_change_handling = false, -- Save/restore sessions when changing directories
-  log_level = "error", -- Sets the log level of the plugin (debug, info, error).
+  args_allow_single_directory = true, -- Follow normal sesion save/load logic if launched with a single directory as the only argument
+  args_allow_files_auto_save = false, -- Allow saving a session even when launched with a file argument (or multiple files/dirs). It does not load any existing session first. While you can just set this to true, you probably want to set it to a function that decides when to save a session when launched with file args. See documentation for more detail
+  continue_restore_on_error = true, -- Keep loading the session even if there's an error. Set to false to get the line number of an error when loading a session
+  cwd_change_handling = false, -- Follow cwd changes, saving a session before change and restoring after
+  log_level = "error", -- Sets the log level of the plugin (debug, info, warn, error).
 
   ---@type SessionLens
   session_lens = {
     load_on_setup = true, -- Initialize on startup (requires Telescope)
-    theme_conf = {}, -- Pass through for Telescope theme options
-    previewer = false, -- File preview for session picker
-
-    ---@type SessionControl
-    session_control = {
-      control_dir = vim.fn.stdpath "data" .. "/auto_session/", -- Auto session control dir, for control files, like alternating between two sessions with session-lens
-      control_filename = "session_control.json", -- File name of the session control file
+    theme_conf = { -- Pass through for Telescope theme options
+      -- layout_config = { -- As one example, can change width/height of picker
+      --   width = 0.8,    -- percent of window
+      --   height = 0.5,
+      -- },
     },
+    previewer = false, -- File preview for session picker
 
     ---@type SessionLensMappings
     mappings = {
       -- Mode can be a string or a table, e.g. {"i", "n"} for both insert and normal mode
       delete_session = { "i", "<C-D>" },
       alternate_session = { "i", "<C-S>" },
+    },
+
+    ---@type SessionControl
+    session_control = {
+      control_dir = vim.fn.stdpath "data" .. "/auto_session/", -- Auto session control dir, for control files, like alternating between two sessions with session-lens
+      control_filename = "session_control.json", -- File name of the session control file
     },
   },
 }
