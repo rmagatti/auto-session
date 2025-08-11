1b6b1918ea6962a58cc74501e2df46354ac62d38
Cameron Ring <cameron@cs.stanford.edu>
2025-08-11 12:50:36 -0700

    tests(preserve_buffer_on_restore): fix windows

--- tests/preserve_buffer_on_restore_spec.lua
+++ tests/preserve_buffer_on_restore_spec.lua
@@ -6,7 +6,7 @@ describe("preserve_buffer_on_restore", function()
 
   as.setup {
     preserve_buffer_on_restore = function(buf_nr)
-      return string.find(vim.api.nvim_buf_get_name(buf_nr), TL.other_file, 1, true) ~= nil
+      return string.find(vim.api.nvim_buf_get_name(buf_nr):gsub("\\", "/"), TL.other_file, 1, true) ~= nil
     end,
   }
 
