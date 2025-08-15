33f7cc2da30a267c658fb3774518243655a68221
Cameron Ring <cameron@cs.stanford.edu>
2025-08-14 22:53:34 -0700

    refactor(pickers): dynamically resolve picker

--- lua/auto-session/pickers/init.lua
+++ lua/auto-session/pickers/init.lua
@@ -10,26 +10,36 @@ local M = {
   picker = nil,
 }
 
----Open a session picker. If Config.session_lens.picker is set, check that.
----Otherwise, check pickers in order to see which is installed
+---Find an available picker. If Config.session_lens.picker is set, check that.
+---Otherwise, check pickers in order to see which is available
 ---Fall back to vim.ui.select
-function M.open_session_picker()
-  if M.picker then
-    return M.picker.open_session_picker()
-  end
-
+---@return Picker # chosen picker
+local function resolve_picker()
   local pickers = Config.session_lens.picker and { Config.session_lens.picker, "select" }
     or { "telescope", "fzf", "snacks", "select" }
 
   for _, name in ipairs(pickers) do
     local ok, picker = pcall(require, "auto-session.pickers." .. name)
     if ok and picker and picker.is_available() then
-      M.picker = picker
-      return M.picker.open_session_picker()
+      Lib.logger.debug("Picking picker: " .. name)
+      return picker
     end
   end
 
+  -- should never get here
   Lib.logger.error "Could not find any pickers?"
+  return require "auto-session.pickers.select"
 end
 
-return M
+function M.open_session_picker()
+  M.picker.open_session_picker()
+end
+
+return setmetatable(M, {
+  __index = function(table, key)
+    if key == "picker" and not rawget(table, "picker") then
+      rawset(table, "picker", resolve_picker())
+    end
+    return rawget(table, key)
+  end,
+})
