662fa7910c6d1943bca50ff9dfe2757f75795197
Cameron Ring <cameron@cs.stanford.edu>
2025-08-17 19:33:45 -0700

    docs(config.lua): update config description

--- lua/auto-session/config.lua
+++ lua/auto-session/config.lua
@@ -87,8 +87,7 @@ local defaults = {
   enabled = true, -- Enables/disables auto creating, saving and restoring
   auto_save = true, -- Enables/disables auto saving session on exit
   auto_restore = true, -- Enables/disables auto restoring session on start
-
-  auto_create = true, -- Enables/disables auto creating new session files. Can take a function that should return true/false if a new session file should be created or not
+  auto_create = true, -- Enables/disables auto creating new session files. Can be a function that returns true if a new session file should be allowed
   auto_restore_last_session = false, -- On startup, loads the last saved session if session for cwd does not exist
   cwd_change_handling = false, -- Automatically save/restore sessions when changing directories
   single_session_mode = false, -- Enable single session mode to keep all work in one session regardless of cwd changes. When enabled, prevents creation of separate sessions for different directories and maintains one unified session. Does not work with cwd_change_handling
@@ -99,7 +98,7 @@ local defaults = {
   bypass_save_filetypes = nil, -- List of filetypes to bypass auto save when the only buffer open is one of the file types listed, useful to ignore dashboards
   close_filetypes_on_save = { "checkhealth" }, -- Buffers with matching filetypes will be closed before saving
   close_unsupported_windows = true, -- Close windows that aren't backed by normal file before autosaving a session
-  preserve_buffer_on_restore = nil, -- return true if a buffer should be preserved when restoring a session
+  preserve_buffer_on_restore = nil, -- Function that returns true if a buffer should be preserved when restoring a session
 
   -- Git
   git_use_branch_name = false, -- Include git branch name in session name
@@ -110,26 +109,26 @@ local defaults = {
   purge_after_minutes = nil, -- Sessions older than purge_after_minutes will be deleted asynchronously on startup, e.g. set to 14400 to delete sessions that haven't been accessed for more than 10 days, defaults to off (no purging), requires >= nvim 0.10
 
   -- Saving extra data
-  save_extra_data = nil, -- Extra data that should be saved with the session. Will be passed to restore_extra_data on restore
-  restore_extra_data = nil, -- Called when there's custom data saved for a session
+  save_extra_data = nil, -- Function that returns extra data that should be saved with the session. Will be passed to restore_extra_data on restore
+  restore_extra_data = nil, -- Function called when there's extra data saved for a session
 
   -- Argument handling
   args_allow_single_directory = true, -- Follow normal session save/load logic if launched with a single directory as the only argument
-  args_allow_files_auto_save = false, -- Allow saving a session even when launched with a file argument (or multiple files/dirs). It does not load any existing session first. While you can just set this to true, you probably want to set it to a function that decides when to save a session when launched with file args. See documentation for more detail
+  args_allow_files_auto_save = false, -- Allow saving a session even when launched with a file argument (or multiple files/dirs). It does not load any existing session first. Can be true or a function that returns true when saving is allowed. See documentation for more detail
 
   -- Misc
   log_level = "error", -- Sets the log level of the plugin (debug, info, warn, error).
   root_dir = vim.fn.stdpath "data" .. "/sessions/", -- Root dir where sessions will be stored
   show_auto_restore_notif = false, -- Whether to show a notification when auto-restoring
-  restore_error_handler = nil, -- Called when there's an error restoring. By default, it ignores fold errors otherwise it displays the error and returns false to disable auto_save
+  restore_error_handler = nil, -- Function called when there's an error restoring. By default, it ignores fold errors otherwise it displays the error and returns false to disable auto_save
   continue_restore_on_error = true, -- Keep loading the session even if there's an error
   lsp_stop_on_restore = false, -- Should language servers be stopped when restoring a session. Can also be a function that will be called if set. Not called on autorestore from startup
-  lazy_support = true, -- Automatically detect if Lazy.nvim is being used and wait until Lazy is done to make sure session is restored correctly. Does nothing if Lazy isn't being used. Can be disabled if a problem is suspected or for debugging
+  lazy_support = true, -- Automatically detect if Lazy.nvim is being used and wait until Lazy is done to make sure session is restored correctly. Does nothing if Lazy isn't being used
 
   ---@type SessionLens
   session_lens = {
-    picker = nil, -- "telescope"|"snacks"|"fzf"|"select"|nil Pickers are detected automatically but you can also manually choose one. Falls back to vim.ui.select
-    load_on_setup = true, -- Only used for telescope, registers the telescope extension startup
+    picker = nil, -- "telescope"|"snacks"|"fzf"|"select"|nil Pickers are detected automatically but you can also set one manually. Falls back to vim.ui.select
+    load_on_setup = true, -- Only used for telescope, registers the telescope extension at startup so you can use :Telescope session-lens
     picker_opts = nil, -- Table passed to Telescope / Snacks / Fzf-Lua to configure the picker. See below for more information
 
     ---@type SessionLensMappings
